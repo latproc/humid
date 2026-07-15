961ec3c0f783fbf9c715176b7e7d20ad29db4891
Mike O'Connor <mjoconr@users.noreply.github.com>
2026-07-16 08:29:31 +0930

    Use Boost-compatible optional check in EditorLabel

--- src/editorlabel.cpp
+++ src/editorlabel.cpp
@@ -40,7 +40,7 @@ bool EditorLabel::mouseButtonEvent(const nanogui::Vector2i &p, int button, bool 
 
     using namespace nanogui;
 
-    if (button == GLFW_MOUSE_BUTTON_LEFT && down && mCallback.has_value()) {
+    if (button == GLFW_MOUSE_BUTTON_LEFT && down && mCallback) {
         (*mCallback)();
     }
 
